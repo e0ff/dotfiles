a3bd949c073ecc3880c9746647b6318a8fbaff61
Jeremy Hertel <dev@jhert.com>
2025-03-20 20:31:11 -0500

    update neovim config and plugins

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -171,7 +171,11 @@ return {
     {
         'tzachar/local-highlight.nvim',
         config = function()
-            require('local-highlight').setup()
+            require('local-highlight').setup({
+                animate = {
+                    enabled = false
+                }
+            })
         end
     },
 }
