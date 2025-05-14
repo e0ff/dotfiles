845d5beb31d833cd6f23ab508871bf1af9b51482
Jeremy Hertel <dev@jhert.com>
2025-05-13 19:31:11 -0500

    neovim: update plugins

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -2,8 +2,8 @@ return {
     {
         'neovim/nvim-lspconfig',
         dependencies = {
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+            { 'mason-org/mason.nvim' },
+            { 'mason-org/mason-lspconfig.nvim' },
 
             { 'hrsh7th/nvim-cmp' },
             { 'hrsh7th/cmp-buffer' },
