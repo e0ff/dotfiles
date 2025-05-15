7469fbe5ea7b452e53105a22895369309d5f76fd
Jeremy Hertel <dev@jhert.com>
2025-05-14 20:39:08 -0500

    add missing change

--- install-windows.ps1
+++ install-windows.ps1
@@ -6,6 +6,5 @@ winget install Neovim.Neovim
 
 New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/nvim/" -Target "$pwd/.config/nvim"
 New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/.emacs.d" -Target "$pwd/.emacs.d"
-New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/wezterm" -Target "$pwd/.config/wezterm"
 New-Item -ItemType SymbolicLink -Path "$profile/.gitconfig" -Target "$pwd/.gitconfig"
 New-Item -ItemType SymbolicLink -Path "$profile/.vsvimrc" -Target "$pwd/.vsvimrc"
