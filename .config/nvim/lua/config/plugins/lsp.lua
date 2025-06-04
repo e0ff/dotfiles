9cbf298113acad3850a9a030d96da226ebda60b3
Jeremy Hertel <dev@jhert.com>
2025-06-03 19:30:46 -0500

    neovim: add keydindings for gd and gD and update plugins

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -43,6 +43,9 @@ return {
                         buffer_autoformat(event.buf)
                     end
 
+                    vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+                    vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
                     vim.api.nvim_command('autocmd CursorHold <buffer> lua vim.diagnostic.open_float({ focusable = false })')
                 end,
             })
