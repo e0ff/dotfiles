3f1ba7ce578c44b6bf63703c8d9bd1bf1c30d424
Jeremy Hertel <e0f@e0f.io>
2026-05-16 12:24:32 -0500

    install global .gitignore on windows

--- install-windows.ps1
+++ install-windows.ps1
@@ -7,4 +7,5 @@ winget install Neovim.Neovim
 New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/nvim/" -Target "$pwd/.config/nvim"
 New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/.emacs.d" -Target "$pwd/.emacs.d"
 New-Item -ItemType SymbolicLink -Path "$profile/.gitconfig" -Target "$pwd/.gitconfig"
+New-Item -ItemType SymbolicLink -Path "$profile/.gitignore_global" -Target "$pwd/.gitignore_global"
 New-Item -ItemType SymbolicLink -Path "$profile/.vsvimrc" -Target "$pwd/.vsvimrc"
