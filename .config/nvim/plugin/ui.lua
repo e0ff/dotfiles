54e3ad814ffb0ef90fc004b45f1e30d2b9e1372a
Jeremy Hertel <e0f@e0f.io>
2026-05-26 21:01:59 -0500

    neovim: add overrides for theme

--- .config/nvim/plugin/ui.lua
+++ .config/nvim/plugin/ui.lua
@@ -3,8 +3,17 @@ vim.pack.add({
     'https://github.com/rmehri01/onenord.nvim',
 })
 
+local colors = require('onenord.colors').load()
+
 require('onenord').setup({
     styles = {
         comments = "italic",
     },
+    custom_highlights = {
+        ["SpellBad"] = { style = 'undercurl', sp = colors.red },
+        ["@type"] = { style = 'bold' },
+        ["@type.builtin"] = { style = 'bold' },
+        ["@keyword.conditional"] = { style = 'italic' },
+        ["@keyword.repeat"] = { style = 'italic' },
+    }
 })
